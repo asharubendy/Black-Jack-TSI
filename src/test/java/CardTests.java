0737898ce9b577f15c2dedc26d912c50943be16d
Ash <Ashbending@gmail.com>
2021-08-24 17:07:14 +0100

    AAA ITS WORKS

--- src/test/java/CardTests.java
+++ src/test/java/CardTests.java
@@ -2,19 +2,28 @@ import com.company.Card;
 import org.junit.jupiter.engine.JupiterTestEngine;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CardTests {
 
     @Test
     @DisplayName("A test to check the instantiation of card works correctly")
     void instantiationTest(){
-
    Card c = new Card(1,"Diamond");
-
    assertEquals(c.getSuit(), "Diamond", "Test failed for assertion of getting suit type");
    assertEquals(c.getCardValue(), 1, "Test failed for assertion of getting card value");
-
     }
+    @Test
+    @DisplayName("A test to check the setters and getters of card")
+    void gettersAndSettersTests(){
+        Card c = new Card(0,"Diamond");
+        c.setCardValue(2);
+        c.setFaceCard(true);
+        c.setFaceCardType("Hearts");
+        assertEquals(c.getCardValue(),c.getCardValue(), "Test failed for assertion of card value");
+        assertTrue(c.isFaceCard(), "Test failed for assertion of is face card value");
+        assertEquals(c.getFaceCardType(),"Hearts", "Test failed for assertion of card type");
+    }
+    
 }
 
